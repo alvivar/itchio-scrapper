76834c2555ea2d303b265f94b3f353dbd6e2a513
Andrés Villalobos <andresalvivar@gmail.com>
2023-08-06 01:14:41 -0600

    A fun optimization!

--- env.py
+++ env.py
@@ -2,13 +2,25 @@ import os
 import sys
 
 
+ENV_CACHE = {}
+
+
 def load(filepath):
+    global ENV_CACHE
+
     frozen = getattr(sys, "frozen", False)  # Pyinstaller
     base_path = sys._MEIPASS if frozen else os.path.dirname(os.path.abspath(__file__))
     env_path = os.path.join(base_path, filepath)
 
-    if os.path.exists(env_path):
-        with open(env_path, "r") as f:
-            for line in f:
-                key, value = line.strip().split("=")
-                os.environ[key.strip()] = value.strip()
+    if env_path not in ENV_CACHE:
+        if os.path.exists(env_path):
+            env = {}
+            with open(env_path, "r") as f:
+                for line in f:
+                    if "=" in line:
+                        key, value = line.strip().split("=")
+                        env[key.strip()] = value.strip()
+
+            ENV_CACHE[env_path] = env
+
+    os.environ.update(ENV_CACHE[env_path])
