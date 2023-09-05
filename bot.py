72ce4eb64f5201cb2fa9b4eddf5bacf5eb67be15
Andrés Villalobos <andresalvivar@gmail.com>
2023-09-04 23:29:20 -0600

    Also download images.

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import json
 
 import db
-from itch import extract_game_info, get_games_from
+from itch import download_images, extract_game_info, get_games_from
 
 
 def dump(data, filename):
@@ -12,6 +12,9 @@ def dump(data, filename):
 if __name__ == "__main__":
     games = get_games_from("https://itch.io/games/top-rated/last-30-days")
     dump(games, "last_itch.json")
+
+    games = download_images(games, "thumbnails")
+    dump(games, "last_itch_images.json")
 
     games_tagged = extract_game_info(games)
     dump(games, "last_itch_tagged.json")
